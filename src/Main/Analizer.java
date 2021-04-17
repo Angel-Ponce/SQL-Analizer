fc96dedcf2b6ac78349694165d0c693947c07539
Angel Ponce <60164099+Angel-Ponce@users.noreply.github.com>
2021-04-17 16:16:02 -0600

    Seconds expresion validate

--- src/Main/Analizer.java
+++ src/Main/Analizer.java
@@ -12,6 +12,8 @@ public class Analizer {
     public static final String FROM = "FROM";
     public static final String ORDERBY = "ORDER BY";
     public static final String WHERE = "WHERE";
+    public static final String ASC = "ASC";
+    public static final String DESC = "DESC";
 
     private String text;
     private JTextArea inputArea;
@@ -25,15 +27,39 @@ public class Analizer {
 
     public void execute() {
         if (text.trim().matches(A)) {
-            String t = text.replaceAll("FROM.*", "").replace("SELECT", "");
-            if (validateSelect(t.trim()).isEmpty()) {
+            String s = text.replaceAll("FROM.*", "").replace("SELECT", "");
+            String t = text.replace("SELECT", "").replace("FROM", "").replace(s, "");
+            if (validateSelect(s.trim()).isEmpty()) {
                 outputArea.setText("SELECCIONAR\n"
-                        + "\t" + t.trim().replace(" ", "") + "\n"
+                        + "\t" + s.trim().replace(" ", "") + "\n"
                         + "DE\n"
-                        + "\t" + text.replace("SELECT", "").replace("FROM", "").replace(t, ""));
+                        + "\t" + t);
+            } else {
+                outputArea.setText(validateSelect(s.trim()));
             }
-        } else if (true) {
+        } else if (text.trim().matches(B)) {
+            String s = text.replaceAll("FROM.*", "").replace("SELECT", "");
+            String t = text.replace("SELECT", "").replace("FROM", "").replace(s, "").replaceAll("ORDER.*", "");
+            if (validateSelect(s.trim()).isEmpty()) {
+                outputArea.setText("SELECCIONAR\n"
+                        + "\t" + s.trim().replace(" ", "") + "\n"
+                        + "DE\n"
+                        + "\t" + t + "\n"
+                        + "ORDENAR " + spanish(text.replace(t, "").replace(s, "").replace("SELECT", "").replace("FROM", "").replace("ORDER", "").replace("BY", "").replace(" ", "")));
+            } else {
+                outputArea.setText(validateSelect(s.trim()));
+            }
+        }
+    }
 
+    public String spanish(String t) {
+        switch (t) {
+            case ASC:
+                return "ASCENDENTEMENTE";
+            case DESC:
+                return "DESCENDENTEMENTE";
+            default:
+                return t;
         }
     }
 
@@ -60,6 +86,6 @@ public class Analizer {
     }
 
     private final String A = "^(SELECT +(.)+ +FROM +\\w+) *$";
-    private final String B = "";
+    private final String B = "^(SELECT +(.)+ +FROM +\\w+) *ORDER *BY *(ASC|DESC) *$";
 
 }
